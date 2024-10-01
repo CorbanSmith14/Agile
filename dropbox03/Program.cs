13a1a68ad3b848ab08e7eaf8f06f3640196eec93
CorbanSmith14 <corbanreismith14@gmail.com>
2024-10-01 13:18:42 -0500

    Add files via upload

---
+++ dropbox03/Program.cs
@@ -0,0 +1,19 @@
+﻿//Input
+Console.WriteLine("How many cookies would you like to buy?");
+//Back End
+
+int numberOfCookies = int.Parse(Console.ReadLine());
+double totalCost; 
+if (numberOfCookies >= 12)
+{
+    int dozens = numberOfCookies / 12;
+    int remainder = numberOfCookies % 12;
+    totalCost = dozens * 1.20 + remainder * 0.12;
+}
+else
+{
+    totalCost = numberOfCookies * 0.12;
+}
+
+//Display
+Console.WriteLine($"The total cost for {numberOfCookies} cookies is ${totalCost:F2}");
