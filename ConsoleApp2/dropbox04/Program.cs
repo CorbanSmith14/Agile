bbcdbfd4f4cee50dacca70d43ed96e87c2bc1d92
CorbanSmith14 <corbanreismith14@gmail.com>
2024-10-01 13:16:53 -0500

    Add files via upload

---
+++ ConsoleApp2/dropbox04/Program.cs
@@ -0,0 +1,17 @@
+﻿//Input
+int[] scores = new int[5];
+//Loop
+for (int i = 0; i < scores.Length; i++)
+{
+    Console.WriteLine("Enter a score:");
+    scores[i] = int.Parse(Console.ReadLine());
+}
+//Display
+Console.WriteLine($"Average: {scores.Average():N2}");
+Console.WriteLine($"Highest: {scores.Max()}");
+Console.WriteLine($"Lowest: {scores.Min()}");
+//Display scores in order
+foreach(int s in scores.OrderByDescending(x=>x))
+{
+    Console.WriteLine(s+" ");
+}
---
+++ dropbox01/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.ForegroundColor = ConsoleColor.Red;
+Console.WriteLine("welcome to Agile");
+Console.Beep();
+Console.BackgroundColor = ConsoleColor.Green;
+Console.WriteLine("welcome again!");
+await Task.Delay(3000);
+Console.Clear();
