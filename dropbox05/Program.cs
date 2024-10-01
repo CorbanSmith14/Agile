ecd722855ab43f8550e79f826a751989523f096f
CorbanSmith14 <corbanreismith14@gmail.com>
2024-10-01 13:20:32 -0500

    Add files via upload

---
+++ dropbox05/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.Xml.Linq;
+using System;
+using System.Text.RegularExpressions;
+//Months of the year
+string[] MONTHS;
+
+MONTHS = new string[12] {"Janruary",
+"Feburary",
+"March",
+"April",
+"May",
+"June",
+"July",
+"August",
+"September",
+"October",
+"November",
+"December", };
+
+int[] rainfall = new int[12];
+
+//Loop tp collect
+//data for rainfall
+for (int i = 0; i < rainfall.Length; i++)
+{
+    Console.WriteLine($"Enter amount of rainfall in inches for {MONTHS[i]} : ");
+    rainfall[i] = int.Parse(Console.ReadLine());
+}
+
+//Display
+Console.WriteLine($"Average Rainfall for the Year (in inches): {rainfall.Average():N2}");
+Console.WriteLine($"Highest Recorded Rainfall for the Year (in inches): {rainfall.Max()}");
+Console.WriteLine($"Lowest Recorded Rainfall for the Year (in inches): {rainfall.Min()}");
+//Display scores in order
+
+foreach (int s in rainfall.OrderByDescending(x => x))
+{
+    Console.WriteLine(s + " ");
+}
---
+++ dropbox06/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Net.Http.Headers;
+
+class Program
+{
+    static void Main ()
+    {
+        Console.WriteLine("What is the first number?");
+        int a = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("What is the second number?");
+        int b = Convert.ToInt32(Console.ReadLine());
+
+        int product = Multiply(a,b);
+        Console.WriteLine("The product is:" + product);
+    }
+
+    static int Multiply (int a, int b) 
+    {
+        int product;
+        product = a * b;
+        return product; 
+    }
+
+}
